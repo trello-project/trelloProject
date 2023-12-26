5d4dfb6e37280f658514490767d3b8247808f236
kkamjjing2 <kkamjjing2@gmail.com>
2023-12-27 04:16:46 +0900

    first commit

---
+++ src/test/java/com/example/trelloproject/TrelloProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trelloproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
