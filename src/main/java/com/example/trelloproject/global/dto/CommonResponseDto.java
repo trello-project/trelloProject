2e4c492f569d24f2425921e1b7941c98082d3a7c
parkjihwan <mankcom7708@gamil.com>
2023-12-29 12:18:01 +0900

    #23 feat : CommonResponseDto

--- src/main/java/com/example/trelloproject/global/dto/CommonResponseDto.java
+++ src/main/java/com/example/trelloproject/global/dto/CommonResponseDto.java
@@ -7,12 +7,15 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class CommonResponseDTO<T> {
+public class CommonResponseDto<T> {
+
     private String message;
+
     private T data;
+
     private Integer statusCode;
 
-    public CommonResponseDTO(String message, Integer statusCode){
+    public CommonResponseDto(String message, Integer statusCode){
         this.message = message;
         this.statusCode = statusCode;
     }
