fba31f462a652830d760e9c8bd5f9be0a9d1b0f7
parkjihwan <mankcom7708@gamil.com>
2023-12-29 19:49:42 +0900

    #12 fix : 댓글 엔티티 수정

--- src/main/java/com/example/trelloproject/Comment/Entity/Comment.java
+++ src/main/java/com/example/trelloproject/Comment/Entity/Comment.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Comment {
 
