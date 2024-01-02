fdb703e6c2e019d28c2a78ad9fabef4940ab1329
yeonwooPark <yeonwoo5115@gmail.com>
2024-01-02 15:11:43 +0900

    #34 feat: add @EnableJpaAuditing

--- src/main/java/com/example/trelloproject/TrelloProjectApplication.java
+++ src/main/java/com/example/trelloproject/TrelloProjectApplication.java
@@ -3,9 +3,11 @@ package com.example.trelloproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
-@SpringBootApplication (exclude = SecurityAutoConfiguration.class)
+@EnableJpaAuditing
+@SpringBootApplication
 public class TrelloProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(TrelloProjectApplication.class, args);
