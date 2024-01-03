fde14fbc0a51ee87afb5a4b72d7a34655b6458f6
parkjihwan <mankcom7708@gmail.com>
2024-01-03 12:33:24 +0900

    #10 fix : AddAssigneeDto 변경

--- src/main/java/com/example/trelloproject/card/dto/AddAssigneeDto.java
+++ src/main/java/com/example/trelloproject/card/dto/AddAssigneeDto.java
@@ -1,10 +1,6 @@
 package com.example.trelloproject.card.dto;
 
-import com.example.trelloproject.user.entity.User;
 import lombok.Getter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 public class AddAssigneeDto {
